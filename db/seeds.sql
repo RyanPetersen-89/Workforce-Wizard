bb3008df9f1f3ed29e284def74683080b9138476
Ryan Petersen <rp26898@gmail.com>
2024-06-26 21:35:49 -0700

    final tweaks to my seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,29 @@
-INSERT INTO department (name) VALUES ('Customer Experience'), ('Bakery'), ('Meat Department'), ('Fresh Food'), ('Leadership');
 
-INSERT INTO role (title, salary, department_id) VALUES ('Clerk', 50000, 1), ('Baker', 55000, 2), ('Deli Butcher', 60000, 3), ('Stocker', 45000, 4), ('Supervisor', 120000, 5);
+INSERT INTO department (name) VALUES 
+('Customer Experience'),
+('Bakery'),
+('Meat Department'),
+('Fresh Food'),
+('Leadership');
+
+
+INSERT INTO role (title, salary, department_id) VALUES 
+('Clerk', 30000, 1), 
+('Baker', 32000, 2), 
+('Deli Butcher', 40000, 3), 
+('Stocker', 30000, 4), 
+('Supervisor', 60000, 5),
+('Lead Supervisor', 65000, 5), 
+('General Manager', 85000, 5);
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-('Sam', 'Barker', 5, 7),
-('June', 'Haverford', 2, 6),
-('Eric', 'Shannon', 3, 6),
-('Brian', 'Grouper', 4, 1),
-('Sal', 'Merrin', 5, 7),
-('Gary', 'Johnson', 5, 7),
-('Scott', 'Michael', 5, NULL), --Highest-level Supervisor other supervisors report to Wayne
-('Gerald', 'White', 1, 5),
-('Maurice', 'Bridgerton', 4, 1);
+('Scott', 'Michael', 7, NULL),  -- Highest-level Supervisor, other supervisors report to Scott
+('Sam', 'Barker', 6, 1),
+('Sal', 'Merrin', 5, 1),
+('Gary', 'Johnson', 5, 1),
+('June', 'Haverford', 2, 3),
+('Eric', 'Shannon', 3, 3),
+('Gerald', 'White', 1, 4),
+('Brian', 'Grouper', 4, 3),
+('Maurice', 'Bridgerton', 4, 3);
